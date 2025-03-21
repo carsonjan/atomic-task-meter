7c7aebad78e7c057e621770fd6d0c954d11b3124
Carson Jan <sc.carson.jan@gmail.com>
2025-03-20 23:03:28 -0700

    Add menuBar

--- src/main/ui/MainGui.java
+++ src/main/ui/MainGui.java
@@ -1,7 +1,11 @@
 package ui;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import ui.gui.MySplitPane;
 
@@ -18,8 +22,29 @@ public class MainGui {
         JComponent newContentPane = new MySplitPane();
         newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
+        frame.setJMenuBar(makeMenuBar());
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    // make menu bar
+    private static JMenuBar makeMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem load = new JMenuItem("Load");
+        file.add(save);
+        file.add(load);
+        menuBar.add(file);
+
+        JMenu about = new JMenu("About");
+        JMenuItem aboutItem = new JMenuItem("About");
+        about.add(aboutItem);
+        menuBar.add(about);
+        
+        menuBar.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+        return menuBar;
     }
 }
