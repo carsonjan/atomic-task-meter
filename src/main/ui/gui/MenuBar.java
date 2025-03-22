2d0315ab94e44c518f6a6ceed41547c578722992
Carson Jan <sc.carson.jan@gmail.com>
2025-03-22 13:43:33 -0700

    Implement save and load data (untested, data unpainted)

--- src/main/ui/gui/MenuBar.java
+++ src/main/ui/gui/MenuBar.java
@@ -5,17 +5,27 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import model.ATM;
+import model.Task;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 public class MenuBar extends JMenuBar implements ActionListener {
     
     private ATM data;
+    private static final String JSON_STORE = "./data/atm.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
 
     // make menu bar
     public MenuBar(ATM data) {
         super();
         this.data = data;
+        this.jsonWriter = new JsonWriter(JSON_STORE);
+        this.jsonReader = new JsonReader(JSON_STORE);
 
         JMenu file = new JMenu("File");
         JMenuItem save = new JMenuItem("Save");
@@ -40,15 +50,46 @@ public class MenuBar extends JMenuBar implements ActionListener {
         switch (source.getText()) {
             case "Save":
                 System.out.println("save!"); // stub
+                saveData();
                 break;
             case "Load":
                 System.out.println("load!"); //stub
+                loadData();
                 break;
             case "About":
                 System.out.println("about!"); //stub
                 break;
             default:
                 // pass (should not reach, do nothing)
+        }
+    }
+
+    // EFFECTS: saves the ATM object to file
+    private void saveData() {
+        Task currentTask = data.getCurrentTask();
+        if (currentTask != null) {
+            Double addTime = data.stopTask(currentTask);
+            System.out.println("> stopped timing: " + currentTask.getName()); // stub
+            System.out.println("> Accumulated an extra " + addTime + "hours"); // stub
+        }
+        try {
+            jsonWriter.open();
+            jsonWriter.write(data);
+            jsonWriter.close();
+            System.out.println("Saved " + " to " + JSON_STORE); // stub
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE); // stub
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads atm from file
+    private void loadData() {
+        try {
+            data = jsonReader.read();
+            System.out.println("Loaded " + " from " + JSON_STORE); // stub
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE); // stub
         }
     }
 }
