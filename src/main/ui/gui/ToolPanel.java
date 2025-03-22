249ac092b3ed634ff758b265bb5988d468b4e2e9
Carson Jan <sc.carson.jan@gmail.com>
2025-03-22 12:54:47 -0700

    Implement addButton (without adding project -> new task panel)

--- src/main/ui/gui/ToolPanel.java
+++ src/main/ui/gui/ToolPanel.java
@@ -12,31 +12,26 @@ public class ToolPanel extends JPanel {
     
     private ATM data;
     private JButton rmButton;
+    private JTextField itemName;
 
     public ToolPanel(ATM data) {
         this.data = data;
+        
+        rmButton = new JButton("🗑️");
+        rmButton.addActionListener(new RmListener());
 
         ImageIcon imgIcon = new ImageIcon("data/plusImage.png");
         imgIcon = (ImageIcon) resizeIcon(imgIcon, 15, 15);
         JButton addButton = new JButton(imgIcon);
 
-        // try {
-        //     // Image img = (Image) new File("../../../../data/plusImage.png");
-        //     addButton.setIcon(new ImageIcon(img));
-        // } catch (Exception ex) {
-        //     System.out.println(ex);
-        // }
-        // HireListener hireListener = new HireListener(hireButton);
-        // addButton.setActionCommand(hireString);
-        // addButton.addActionListener(hireListener);
-        // addButton.setEnabled(false);
+        AddListener addListener = new AddListener(addButton);
+        addButton.setActionCommand("add");
+        addButton.addActionListener(addListener);
+        addButton.setEnabled(false);
 
-        rmButton = new JButton("🗑️");
-        rmButton.addActionListener(new RmListener());
-
-        JTextField itemName = new JTextField(10);
-        // itemName.addActionListener(hireListener);
-        // itemName.getDocument().addDocumentListener(hireListener);
+        itemName = new JTextField(10);
+        itemName.addActionListener(addListener);
+        itemName.getDocument().addDocumentListener(addListener);
         // String name = listModel.getElementAt(
         //                       list.getSelectedIndex()).toString();
 
@@ -44,11 +39,6 @@ public class ToolPanel extends JPanel {
         add(itemName);
         add(addButton);
 
-        // stub
-        // JLabel testLabel = new JLabel("This is a tool panel");
-        // testLabel.setBackground(Color.RED);
-        // testLabel.setOpaque(true);
-        // add(testLabel);
     }
 
     private static Icon resizeIcon(ImageIcon icon, int resizedWidth, int resizedHeight) {
@@ -90,6 +80,92 @@ public class ToolPanel extends JPanel {
                 list.setSelectedIndex(index);
                 list.ensureIndexIsVisible(index);
             }
+        }
+    }
+
+    //This listener is shared by the text field and the hire button.
+    class AddListener implements ActionListener, DocumentListener {
+        private boolean alreadyEnabled = false;
+        private JButton button;
+
+        public AddListener(JButton button) {
+            this.button = button;
+        }
+
+        //Required by ActionListener.
+        public void actionPerformed(ActionEvent e) {
+            String name = itemName.getText();
+            MyScrollPane itemPane = getMyParent().getItemPane();
+            JList list = itemPane.getList();
+            DefaultListModel listModel = itemPane.getListModel();
+
+            //User didn't type in a unique name...
+            if (name.equals("") || alreadyInList(name)) {
+                Toolkit.getDefaultToolkit().beep();
+                itemName.requestFocusInWindow();
+                itemName.selectAll();
+                return;
+            }
+
+            int index = list.getSelectedIndex(); //get selected index
+            if (index == -1) { //no selection, so insert at beginning
+                index = 0;
+            } else {           //add after the selected item
+                index++;
+            }
+
+            listModel.insertElementAt(itemName.getText(), index);
+            //If we just wanted to add to the end, we'd do this:
+            //listModel.addElement(employeeName.getText());
+
+            //Reset the text field.
+            itemName.requestFocusInWindow();
+            itemName.setText("");
+
+            //Select the new item and make it visible.
+            list.setSelectedIndex(index);
+            list.ensureIndexIsVisible(index);
+        }
+
+        //This method tests for string equality. You could certainly
+        //get more sophisticated about the algorithm.  For example,
+        //you might want to ignore white space and capitalization.
+        protected boolean alreadyInList(String name) {
+            MyScrollPane itemPane = getMyParent().getItemPane();
+            DefaultListModel listModel = itemPane.getListModel();
+            return listModel.contains(name);
+        }
+
+        //Required by DocumentListener.
+        public void insertUpdate(DocumentEvent e) {
+            enableButton();
+        }
+
+        //Required by DocumentListener.
+        public void removeUpdate(DocumentEvent e) {
+            handleEmptyTextField(e);
+        }
+
+        //Required by DocumentListener.
+        public void changedUpdate(DocumentEvent e) {
+            if (!handleEmptyTextField(e)) {
+                enableButton();
+            }
+        }
+
+        private void enableButton() {
+            if (!alreadyEnabled) {
+                button.setEnabled(true);
+            }
+        }
+
+        private boolean handleEmptyTextField(DocumentEvent e) {
+            if (e.getDocument().getLength() <= 0) {
+                button.setEnabled(false);
+                alreadyEnabled = false;
+                return true;
+            }
+            return false;
         }
     }
 }
